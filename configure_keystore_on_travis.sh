728f5431a05eef526ab8672ec4fd2b4ee16bfae5
Youenn PENNARUN <youenn.pennarun@gmail.com>
2016-11-26 19:14:35 +0000

    Update keystore

--- configure_keystore_on_travis.sh
+++ configure_keystore_on_travis.sh
@@ -1,6 +1,6 @@
 if [ $# -eq 3 ]
 then
-	travis encrypt-file mobile_app/android/release.keystore --add
+	travis encrypt-file mobile_app/android/app/release.keystore --add
 	travis encrypt MYAPP_RELEASE_KEY_ALIAS=$1 --add env.global
 	travis encrypt MYAPP_RELEASE_STORE_PASSWORD=$2 --add env.global
 	travis encrypt MYAPP_RELEASE_KEY_PASSWORD=$3 --add env.global
