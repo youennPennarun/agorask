757f62451724c85b13ad5f5ae4ea08d1393a2da6
Youenn PENNARUN <youenn.pennarun@gmail.com>
2016-11-26 03:54:28 +0000

    Add encrypted keystore for travis

---
+++ mobile_app/configure_keystore_on_travis.sh
@@ -0,0 +1,11 @@
+if [ $# -eq 3 ]
+then
+	travis encrypt-file release.keystore --add
+	travis encrypt MYAPP_RELEASE_KEY_ALIAS=$1 --add env.global
+	travis encrypt MYAPP_RELEASE_STORE_PASSWORD=$2 --add env.global
+	travis encrypt MYAPP_RELEASE_KEY_PASSWORD=$3 --add env.global
+	echo "stored";
+else
+	echo "Should take 3 parameters: key_alias store_password key_password";
+fi
+
