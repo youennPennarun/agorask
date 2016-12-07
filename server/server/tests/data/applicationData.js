034fda1ccd8431365222443f7d9bd930274b2d71
Youenn PENNARUN <youenn.pennarun@gmail.com>
2016-12-07 15:20:29 +0000

    Add application data for the tests

---
+++ server/server/tests/data/applicationData.js
@@ -0,0 +1,21 @@
+module.exports = [
+  {
+    _id: '58481f0831858d7e0cc8835a',
+    releaseDate: new Date('01/01/03'),
+    version: '0.0.1',
+    type: 'release',
+    boxId: '999999',
+    fileName: 'agorask.apk',
+    downloadUrl: 'http://test/download/apk/release',
+  },
+  {
+    _id: '58481e6c5cd86e7d2588d74c',
+    releaseDate: new Date('01/02/03'),
+    version: '0.0.1',
+    type: 'dev',
+    boxId: '999998',
+    fileName: 'agorask.apk',
+    downloadUrl: 'http://test/download/apk/dev',
+  },
+
+];
