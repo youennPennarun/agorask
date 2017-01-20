63d49d9558f55f59a8fdb4a8587e376b29e34d3c
Youenn PENNARUN <youenn.pennarun@gmail.com>
2017-01-20 15:59:50 +0000

    CI: fix start emulator cmd

--- tools/buildApplication/buildNativeVersion.js
+++ tools/buildApplication/buildNativeVersion.js
@@ -10,30 +10,41 @@ const args = require('./cmdArgs');
 const configs = require('./config.js');
 const config = configs.branches[args['branch']];
 
-
 const token = process.env.ADMIN_TOKEN;
 
 function* getVersion(ANDROID_PATH, buildType) {
   console.log(`${colors.blue('getting current version')}`);
   const reGetVersion = /android {[\s\S]*defaultConfig {[\s\S]*versionName "(.*)"/;
-  const reGetSuffix = new RegExp(`buildTypes {[\\s\\S]*${buildType} {[^}]*versionNameSuffix "(.*)"[^}]*}`)
-  const gradleConfig = fs.readFileSync(`${ANDROID_PATH}/app/build.gradle`).toString();
+  const reGetSuffix = new RegExp(
+    `buildTypes {[\\s\\S]*${buildType} {[^}]*versionNameSuffix "(.*)"[^}]*}`,
+  );
+  const gradleConfig = fs
+    .readFileSync(`${ANDROID_PATH}/app/build.gradle`)
+    .toString();
   const versionMatched = gradleConfig.match(reGetVersion);
   const suffixMatched = gradleConfig.match(reGetSuffix);
-  const versionName = (versionMatched) ? versionMatched[1] : '0.0.0';
-  const suffix = (suffixMatched) ? suffixMatched[1] : '';
-  console.log(`${colors.white('Got version')} ${colors.green(`${versionName}${suffix}`)}`);
-  return `${versionName}${suffix}`
+  const versionName = versionMatched ? versionMatched[1] : '0.0.0';
+  const suffix = suffixMatched ? suffixMatched[1] : '';
+  console.log(
+    `${colors.white('Got version')} ${colors.green(`${versionName}${suffix}`)}`,
+  );
+  return `${versionName}${suffix}`;
 }
 
 function* uploadRelease(ANDROID_PATH, releaseDate) {
   const version = yield getVersion(ANDROID_PATH, config.buildType);
   const url = `${configs.deployTo}/application/${releaseDate}?version=${version}&type=${config.buildType}`;
-  console.log(`Uploading version ${version} on ${url}...`.green)
+  console.log(`Uploading version ${version} on ${url}...`.green);
   const form = new FormData();
-  form.append('app', fs.createReadStream(`${ANDROID_PATH}/app/build/outputs/apk/${config.out}`));
+  form.append(
+    'app',
+    fs.createReadStream(`${ANDROID_PATH}/app/build/outputs/apk/${config.out}`),
+  );
   try {
-    const response = yield fetch(`${url}&token=${token}`, { method: 'POST', body: form });
+    const response = yield fetch(`${url}&token=${token}`, {
+      method: 'POST',
+      body: form,
+    });
     if (response.status === 200) {
       console.log('Application successfully uploaded'.green);
     } else {
@@ -49,26 +60,27 @@ function* uploadRelease(ANDROID_PATH, releaseDate) {
 
 function buildRelease(ANDROID_PATH, releaseDate) {
   return new Promise((resolve, reject) => {
-    const env = Object.assign({}, process.env, {
-      'ENVFILE': config.envFile,
-    })
-    const options = {
-      cwd: `${ANDROID_PATH}`,
-      env: env
-    }
-    const p = spawn('./gradlew', [config.gradleCommand, "--stacktrace", "--info"], options);
-    p.childProcess.stdout.on('data', (data) => {
+    const env = Object.assign({}, process.env, {ENVFILE: config.envFile});
+    const args = [config.gradleCommand, '--stacktrace', '--info'];
+    const options = {cwd: `${ANDROID_PATH}`, env: env};
+    console.log(`Running ./gradlew ${args.join(' ')}`);
+    const p = spawn('./gradlew', args, options);
+    p.childProcess.stdout.on('data', data => {
       process.stdout.write(data.toString());
     });
-    p.childProcess.stderr.on('data', (data) => {
-      process.stdout.write(data.toString())
+    p.childProcess.stderr.on('data', data => {
+      process.stdout.write(data.toString());
     });
-    p.then(() => {
-      co(uploadRelease(ANDROID_PATH, releaseDate)).then(resolve).catch(reject);
-    }).catch(e => {
-      console.log('error', e);
-      reject();
-    });
+    p
+      .then(() => {
+        co(uploadRelease(ANDROID_PATH, releaseDate))
+          .then(resolve)
+          .catch(reject);
+      })
+      .catch(e => {
+        console.log('error', e);
+        reject();
+      });
   });
 }
 
