2c772e70effed9a8e91e940a20adf09120efb16a
Youenn PENNARUN <youenn.pennarun@gmail.com>
2017-01-20 15:02:39 +0000

    install abi

--- tools/android_deps.sh
+++ tools/android_deps.sh
@@ -12,7 +12,7 @@ if [ ! -e $DEPS ]; then
   echo y | android update sdk -u -a -t extra-google-m2repository &&
   echo y | android update sdk -u -t extra-android-m2repository &&
   echo y | android update sdk -u -t extra-android-support &&
-  echo y | android update sdk -a -u -t sys-img-armeabi-v7a-google_apis-22 &&
-  echo no | android create avd -n testAVD -f -t android-23 --abi google_apis/armeabi-v7a
+  echo y | android update sdk -a -u -t sys-img-armeabi-v7a-android-23 &&
+  echo no | android create avd -n testAVD -f -t android-23 --abi armeabi-v7a
   touch $DEPS
 fi
