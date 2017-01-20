25ad8ee33fca597c955e3b8414905b6db02307cf
Youenn PENNARUN <youenn.pennarun@gmail.com>
2017-01-20 13:55:07 +0000

    Cache android dependencies on CircleCI

--- tools/android_deps.sh
+++ tools/android_deps.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+# export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 
 DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
-  cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
   echo y | android update sdk -u -a -t tools &&
   echo y | android update sdk -u -a -t build-tools-23.0.3 &&
   echo y | android update sdk -u -a -t android-23 &&
