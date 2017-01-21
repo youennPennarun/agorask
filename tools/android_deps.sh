baf699fd34669f1ef59791d317bc9abc5b0cebcc
Youenn PENNARUN <youenn.pennarun@gmail.com>
2017-01-21 00:58:35 +0000

    CI: fix $PATH

--- tools/android_deps.sh
+++ tools/android_deps.sh
@@ -3,7 +3,7 @@
 # export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 
 DEPS="$ANDROID_HOME/installed-dependencies"
-export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+
 if [ ! -e $DEPS ]; then
   echo y | android update sdk -u -a -t tools &&
   echo y | android update sdk -u -a -t build-tools-23.0.1 &&
