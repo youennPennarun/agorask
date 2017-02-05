0222de41dc6d8e104f57cf78d91f5cee4ec9f189
Youenn PENNARUN <youenn.pennarun@gmail.com>
2017-02-05 17:56:00 +0000

    remove broken scaleToSameSize

--- tests/app/features/step_definitions/test.js
+++ tests/app/features/step_definitions/test.js
@@ -29,7 +29,6 @@ defineSupportCode(function({Given, When, Then}) {
           resemble(fileFullName)
             .compareTo(newFileFullName)
             .ignoreRectangles([[0,0,10000, 75]])
-            .scaleToSameSize()
             .ignoreAntialiasing()
             .onComplete(function(data){
               console.log(data);
