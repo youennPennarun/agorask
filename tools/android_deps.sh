3987a5e8f61c8a2d0aa86511023d3b58406bf7d6
Youenn PENNARUN <youenn.pennarun@gmail.com>
2017-05-06 20:34:43 +0100

    :construction: add android build tool 25

--- tools/android_deps.sh
+++ tools/android_deps.sh
@@ -8,6 +8,7 @@ if [ ! -e $DEPS ]; then
   echo y | android update sdk -u -a -t tools &&
   echo y | android update sdk -u -a -t build-tools-23.0.1 &&
   echo y | android update sdk -u -a -t build-tools-23.0.3 && 
+  echo y | android update sdk -u -a -t build-tools-25.0.0 && 
   echo y | android update sdk -u -a -t android-23 &&
   echo y | android update sdk -u -a -t android-25 &&
   echo y | android update sdk -u -a -t extra-google-google_play_services &&
